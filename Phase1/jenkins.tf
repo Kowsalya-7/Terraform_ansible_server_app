8dc3ce438e2e0b87a02ed96e33659e13669e0995
Kowsalya-7 <128200470+Kowsalya-7@users.noreply.github.com>
2025-08-26 12:07:33 +0530

    Update jenkins.tf

--- Phase1/jenkins.tf
+++ Phase1/jenkins.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins_server" {
   instance_type = "t3.medium"
   subnet_id     = aws_subnet.cloud_hustlers_subnet.id
   security_groups = [aws_security_group.cloud_hustlers_sg.id]
-  key_name      = "cloudhustlers"  # replace with your AWS key pair
+  key_name      = "cloud_hustlers"  # replace with your AWS key pair
 
   tags = { Name = "cloudhustlers-jenkins-server" }
 }
